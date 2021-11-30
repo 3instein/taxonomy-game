6eedcb8ef244b9d8c08650419c2d06625e914ce4
Michael Eco <michael.gunawan2002@gmail.com>
2021-11-30 20:42:06 +0700

    Fix dna animation

--- resources/views/livewire/click.blade.php
+++ resources/views/livewire/click.blade.php
@@ -1,9 +1,13 @@
-<div class="canvas">
+<div class="canvas d-flex flex-column align-items-center justify-content-evenly">
     <div id="dna">
         <img src="{{ asset('assets/dna.png') }}">
     </div>
-</div>
-<div class="d-flex flex-column align-items-center justify-content-evenly" style="min-height: 100vh"">
+    <div id="dna">
+        <img src="{{ asset('assets/dna.png') }}">
+    </div>
+    <div id="dna">
+        <img src="{{ asset('assets/dna.png') }}">
+    </div>
     <h1 class="score">{{ auth()->user()->userSave->evo }}</h1>
     <div wire:click="increment" class="circle"></div>
 </div>
