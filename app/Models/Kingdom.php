aa9c0e760232c3c13e4e41468c4989fa4d64adab
Michael Eco <michael.gunawan2002@gmail.com>
2021-11-30 08:12:09 +0700

    Initial model commit

--- app/Models/Kingdom.php
+++ app/Models/Kingdom.php
@@ -5,15 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Save extends Model
-{
+class Kingdom extends Model {
     use HasFactory;
 
-    protected $guarded = [
-        'id'
-    ];
+    protected $guarded = ['id'];
 
-    public function user(){
-        return $this->belongsTo(User::class);
+    public function domain() {
+        return $this->belongsTo(Domain::class);
     }
 }
---
+++ app/Models/Species.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Species extends Model {
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function genus() {
+        return $this->belongsTo(Genus::class);
+    }
+
+    public function saveDetails() {
+        return $this->hasMany(SaveDetail::class);
+    }
+}
