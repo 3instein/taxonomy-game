d58d9d6299f4441d3ad130a2849d149e6ff073f5
Reynaldi Kindarto <37333204+3instein@users.noreply.github.com>
2021-11-30 20:21:40 +0700

    Update click.blade.php

--- resources/views/livewire/click.blade.php
+++ resources/views/livewire/click.blade.php
@@ -1,11 +1,3 @@
-{{-- <div wire:click="increment" class="d-flex flex-column align-items-center justify-content-evenly"
-    style="min-height: 100vh"">
-    <h1 class="score">{{ $count }}</h1>
-    <div class="circle"></div>
-    <div id="object-follow">
-        <h3 id="popup-text">1</h3>
-    </div>
-</div> --}}
 <div class="canvas">
     <div id="dna">
         <img src="{{ asset('assets/dna.png') }}">
