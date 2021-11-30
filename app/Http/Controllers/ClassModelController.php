aa1a4b49f6b80b6035f5a98f48a8f311b0be9a3d
Michael Eco <michael.gunawan2002@gmail.com>
2021-11-30 08:14:37 +0700

    Initial controller commit

---
+++ app/Http/Controllers/ClassModelController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ClassModel;
+use Illuminate\Http\Request;
+
+class ClassModelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ClassModel  $classModel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ClassModel $classModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ClassModel  $classModel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ClassModel $classModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ClassModel  $classModel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ClassModel $classModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ClassModel  $classModel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ClassModel $classModel)
+    {
+        //
+    }
+}
