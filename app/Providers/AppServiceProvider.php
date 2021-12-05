40847ea5b74aef7ea58683506641d132b3603d3d
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2021-12-05 19:15:33 +0700

    Fix AppServiceProvider namespace

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
