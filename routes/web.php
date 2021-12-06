20c0d6b85eded582830558e7a3ac6ce56f525049
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2021-12-06 09:48:10 +0700

    Add middleware Auth

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', App::class);
+Route::get('/', App::class)->middleware('auth');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
