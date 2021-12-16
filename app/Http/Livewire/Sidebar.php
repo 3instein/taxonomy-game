86a724d474877649ab800a10e7a5edd360dbe658
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2021-12-16 09:06:31 +0700

    Creature Unlock Validation

--- app/Http/Livewire/Sidebar.php
+++ app/Http/Livewire/Sidebar.php
@@ -3,12 +3,21 @@
 namespace App\Http\Livewire;
 
 use App\Models\Species;
+use App\Models\UserCreature;
+use Illuminate\Database\Eloquent\Builder;
 use Livewire\Component;
 
 class Sidebar extends Component {
+
+    public $species;
+
     public function render() {
+        $this->species = Species::whereDoesntHave('creature', function(Builder $query){
+            $query->where('student_id', auth()->user()->id);
+        })->get();
+
         return view('livewire.sidebar', [
-            'species' => Species::all()
+            'species' => $this->species
         ]);
     }
 }
