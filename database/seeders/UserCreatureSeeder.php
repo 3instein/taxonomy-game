fdee48bc350e2deb6a27fffa7f50abb64a8ecf62
ssabrut <michael.gunawan2002@gmail.com>
2021-12-29 11:26:07 +0700

    Fix sidebar creature image

--- database/seeders/UserCreatureSeeder.php
+++ database/seeders/UserCreatureSeeder.php
@@ -13,23 +13,23 @@ class UserCreatureSeeder extends Seeder {
      * @return void
      */
     public function run() {
-        UserCreature::create([
-            'student_id' => 1,
-            'species_id' => 1,
-            'amount' => 1
-        ]);
+        // UserCreature::create([
+        //     'student_id' => 1,
+        //     'species_id' => 1,
+        //     'amount' => 1
+        // ]);
 
-        UserCreature::create([
-            'student_id' => 1,
-            'species_id' => 2,
-            'amount' => 1
-        ]);
+        // UserCreature::create([
+        //     'student_id' => 1,
+        //     'species_id' => 2,
+        //     'amount' => 1
+        // ]);
 
-        foreach (range(1, 6) as $index) {
-            DB::table('user_creatures_evolutions')->insert([
-                'user_creature_id' => rand(1, 2),
-                'creature_evolution_id' => $index
-            ]);
-        }
+        // foreach (range(1, 6) as $index) {
+        //     DB::table('user_creatures_evolutions')->insert([
+        //         'user_creature_id' => rand(1, 2),
+        //         'creature_evolution_id' => $index
+        //     ]);
+        // }
     }
 }
