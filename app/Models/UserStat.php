b0ac7d626f3811c3e44956aa6a2a1a6cf3a0eee6
ssabrut <michael.gunawan2002@gmail.com>
2022-01-01 11:19:36 +0700

    Add user stat api

--- app/Models/UserStat.php
+++ app/Models/UserStat.php
@@ -5,15 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class UserStat extends Model
-{
+class UserStat extends Model {
     use HasFactory;
 
     protected $table = 'user_stats';
 
     protected $guarded = [''];
 
-    public function user(){
+    public function user() {
         return $this->belongsTo(User::class, 'student_id', 'student_id');
     }
 }
