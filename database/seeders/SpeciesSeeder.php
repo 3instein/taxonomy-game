994ae955247e4ff91f0d85081dae45da6c3e6afe
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2022-01-02 10:28:02 +0700

    Add common name field to SpeciesSeeder

--- database/seeders/SpeciesSeeder.php
+++ database/seeders/SpeciesSeeder.php
@@ -16,6 +16,7 @@ class SpeciesSeeder extends Seeder {
         Species::create([
             'genus_id' => 1,
             'name' => 'Sponge Sp',
+            'common_name' => 'Bunga Karang',
             'price' => 1,
             'description' => 'Organisme multiseluler pertama di dunia. Bunga karang adalah organisme yang diam di tempat dan menyerap nutrisi menggunakan sebuah filter untuk menyaring makanan. Bunga karang terbentuk dari banyak sel eukariotik yang menjadi satu organisme.',
             'image_path' => 'http://taxonomy-game-web.test/assets/species/sponge-sp.jpg'
@@ -23,7 +24,8 @@ class SpeciesSeeder extends Seeder {
 
         Species::create([
             'genus_id' => 2,
-            'name' => 'Mnemiopsis iedyi',
+            'name' => 'Mnemiopsis Iedyi',
+            'common_name' => 'Ubur-ubur',
             'price' => 1,
             'description' => 'Ubur-ubur adalah organisme laut bertubuh lunak dengan tubuh berbentuk payung dengan tekstur mirip gelatin dilengkapi dengan tentakel. Mereka bisa berenang melalui air dengan mendenyutkan tubuh mereka.',
             'image_path' => 'http://taxonomy-game-web.test/assets/species/mnemiopsis-iedyi.jpg',
