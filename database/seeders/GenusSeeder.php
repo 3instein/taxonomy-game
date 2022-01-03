58b9957abf826fb2e20e19347f912adcf993d5e0
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2022-01-03 09:13:22 +0700

    Add cacing pipih SpeciesSeeder and EvolutionSeeder

--- database/seeders/GenusSeeder.php
+++ database/seeders/GenusSeeder.php
@@ -12,56 +12,6 @@ class GenusSeeder extends Seeder {
      * @return void
      */
     public function run() {
-        // Genus::create([
-        //     'family_id' => 1,
-        //     'name' => 'Homo'
-        // ]);
-
-        // Genus::create([
-        //     'family_id' => 2,
-        //     'name' => 'Felis'
-        // ]);
-
-        // Genus::create([
-        //     'family_id' => 3,
-        //     'name' => 'Canis'
-        // ]);
-
-        // Genus::create([
-        //     'family_id' => 4,
-        //     'name' => 'Lumbricus'
-        // ]);
-
-        // Genus::create([
-        //     'family_id' => 5,
-        //     'name' => 'Orcinus'
-        // ]);
-
-        // Genus::create([
-        //     'family_id' => 6,
-        //     'name' => 'Rosa'
-        // ]);
-
-        // Genus::create([
-        //     'family_id' => 7,
-        //     'name' => 'Bambusa'
-        // ]);
-
-        // Genus::create([
-        //     'family_id' => 8,
-        //     'name' => 'Helianthus'
-        // ]);
-
-        // Genus::create([
-        //     'family_id' => 9,
-        //     'name' => 'Nymphaea'
-        // ]);
-
-        // Genus::create([
-        //     'family_id' => 10,
-        //     'name' => 'Bauhinia'
-        // ]);
-
         Genus::create([
             'family_id' => 1,
             'name' => 'Spongia Linnaeus'
